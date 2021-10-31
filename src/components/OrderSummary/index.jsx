ddabff6c5b6644442c2ef655b7fcf0d69c864ec3
abizmo <abizmo@gmail.com>
2021-10-31 16:25:57 +0000

    feat: initial commit

---
+++ src/components/OrderSummary/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const OrderSummary = () => (
+  <div>
+    Order Summary
+
+    You can now listen to millions of songs, audiobooks, and podcasts on any
+    device anywhere you like!
+
+    Annual Plan
+    $59.99/year
+
+    Change
+
+    Proceed to Payment
+    Cancel Order
+
+    <div className="attribution">
+      Challenge by
+      {' '}
+      <a href="https://www.frontendmentor.io?ref=challenge" rel="noreferrer" target="_blank">Frontend Mentor</a>
+      .
+      Coded by
+      {' '}
+      <a href="https://www.abizmo.dev">ab!Zmo</a>
+      .
+    </div>
+  </div>
+);
+
+export default OrderSummary;
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import OrderSummary from './components/OrderSummary';
+
+ReactDOM.render(
+  <OrderSummary />,
+  document.getElementById('root'),
+);
