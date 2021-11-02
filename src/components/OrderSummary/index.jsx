a05762f17134e60081322beffdb292c55a631f86
abizmo <abizmo@gmail.com>
2021-11-02 18:19:01 +0000

    style: test if ghpages load last version

--- src/components/OrderSummary/index.jsx
+++ src/components/OrderSummary/index.jsx
@@ -17,7 +17,7 @@ const OrderSummary = () => (
         <Feature.Icon src="./images/icon-music.svg" />
         <Feature.Info>
           <Feature.Title>
-            Annual Plan
+            Annual Plax
           </Feature.Title>
           <Feature.Text>
             $59.99/year
